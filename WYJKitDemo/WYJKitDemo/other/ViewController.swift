7fbaabd6c4add7f335170026cd55fc1456cfb917
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2021-03-25 14:40:38 +0800

    1.1.3

--- WYJKitDemo/WYJKitDemo/other/ViewController.swift
+++ WYJKitDemo/WYJKitDemo/other/ViewController.swift
@@ -32,24 +32,6 @@ class ViewController: WYJBaseViewController  {
         WYJLog(sss)
         
     }
-    func calculateDifference(startTime: String,endTime: String) -> Bool {
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 8)
-        dateFormatter.dateFormat = "yyyyMMdd"
-
-        if let date1 = dateFormatter.date(from: startTime),let date2 = dateFormatter.date(from: endTime) {
-            let components = NSCalendar.current.dateComponents([.year,.month,.day], from: date1, to: date2)
-            if let year = components.year {
-                if year <= 59 && year >= 18 {
-                    return true
-                }
-            }
-        } else {
-            
-        }
-        return false
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let d =  0.123
@@ -70,7 +52,6 @@ class MainInstructions: UIView {
             $0.backgroundColor = .gray
             $0.numberOfLines = 0
             $0.font = WYJFont(20)
-            $0.text = "产品说明/1、设备服务费：298元/台，首笔需支付298元的设备服务费方可激活使用本产品 2、流量卡费：36元/年，激活后第31天的首笔交易扣36元的流量卡费，激活后的396天的首笔交易扣第二年的36元流量卡费用以此类推 3、交易到账时间：DO交易：00:00-22:40、23:05-23:59，到账时间：实时到账小额双免、扫码消费：09:00-22:00，到账时间：实时到账4、交易限额:信用卡：最低10元/笔，最高5万元/笔，单卡单日限5笔储蓄卡：最低10元/笔，最高5000元/笔，单卡单日限2笔云闪付扫码：单笔限额5000元，单日限10笔支付宝扫码：单笔限额1000元，单日限3笔单日刷卡限额50万元，当月刷卡限额300万元。5、认证规则:为了您的资金安全，磁条卡首次大额交易（5000元以上）需进行持卡人四要素（持卡人姓名、身份证号、银行卡号、银行预留手机号码）认证，否则无法完成交易。认证通过后单笔最高5万元。6、声明：使用本产品服务需要真实的交易背景，用户需要自行承担各类交易税费。严禁用户利用本公司产品做违法行为，否则后果自负。"
         })
     }()
     @objc func injected() {
